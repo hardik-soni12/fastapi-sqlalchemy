4701dd0f1a25f44b5f80ef1d5c5c2f03cc3e9185
Hardik-Soni12 <imailhr1k@gmail.com>
2026-01-31 16:11:29 +0530

    Bump version to 0.1.2 for automation test

--- fastapi_sqlalchemy_ease/__init__.py
+++ fastapi_sqlalchemy_ease/__init__.py
@@ -1,7 +1,7 @@
 from .core import SQLAlchemy
 from .exceptions import DatabaseError, DatabaseNotInitializedError
 
-__version__='0.1.2'
+__version__='0.1.3'
 
 # when someone call: from fastapi_sqlalchemy import SQLAlchemy
 # only these things are available
